5bf97e47bfaa6389fb8a03d115968fb9b918c215
Donovan Sohr <donovansohr@Donovans-MacBook-Pro.local>
2018-10-02 22:10:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
 
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardsInPlay = [];
+//From here up, it is good to go
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+     	alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
