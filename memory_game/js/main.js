36e8a5cba1707f12a77e8540d78ca942a8924d92
Donovan Sohr <donovansohr@Donovans-MacBook-Pro.local>
2018-10-06 13:37:35 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,24 @@
+
 var cards = [
 {
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png",
 },
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png",
 },
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png",
 },
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png",
 }
 ];
 
@@ -41,9 +42,6 @@ var flipCard = function () {
     };
 };
 console.log(cardsInPlay);
-console.log("User flipped " + cards[cardId].rank);
-console.log("User flipped " + cards[cardId].cardImage);
-console.log("User flipped " + cards[cardId].suit);
 
 
 var createBoard = function () {
@@ -53,7 +51,7 @@ var createBoard = function () {
      cardElement.setAttribute('src', 'images/back.png');
      cardElement.setAttribute('data-id', i);
      cardElement.addEventListener('click', flipCard);
-     cardElement.appendChild(game-board);
+     cardTable.appendChild(cardElement);
     	//correct order?
 }
 };
